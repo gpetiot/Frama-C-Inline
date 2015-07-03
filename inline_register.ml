2941f7dd4cadd900d5f92baf10861a05de3b562e
Guillaume Petiot <guillaume.petiot@cea.fr>
2015-07-03 10:25:57 +0200

    do not use class methods to inline

--- inline_register.ml
+++ inline_register.ml
@@ -2,186 +2,189 @@
 open Cil_types
 
 
-(* Make substitutions in terms and predicates
- * 
- * Bindings:
- * ll: logic_label => logic_label
- * vt: logic_var => term
- * vp: logic_var => predicate
- * vv: logic_var => logic_var
- *)
-class subst ?subst_result () = object(self)
+type env = {
+  label_assoc : (logic_label * logic_label) list;
+  term_assoc : (logic_var * term) list;
+  pred_assoc : (logic_var * predicate) list;
+  var_assoc : (logic_var * logic_var) list;
+}
 
-  method app ll vt vv li lassoc params =
-    let rec aux_label ret = function
-      | [] -> ret
-      | (x,y)::t -> aux_label ((x, self#label y ll)::ret) t
-    in
-    let rec aux_arg ret = function
-      | [], [] -> ret
-      | x::t1, y::t2 -> aux_arg ((x, self#term y ll vt vv)::ret) (t1,t2)
-      | _ -> assert false
-    in
-    aux_label [] lassoc, aux_arg [] (li.l_profile, params)
+let empty_env = {label_assoc=[]; term_assoc=[]; pred_assoc=[]; var_assoc=[];}
 
-  method pred pred ll vt vp vv = match pred with
+let label env l =
+  if List.mem_assoc l env.label_assoc then List.assoc l env.label_assoc else l
+
+let rec app env li lassoc params =
+  let rec aux_label ret = function
+    | [] -> ret
+    | (x,y)::t -> aux_label ((x, label env y)::ret) t
+  in
+  let rec aux_arg ret = function
+    | [], [] -> ret
+    | x::t1, y::t2 -> aux_arg ((x, term env y)::ret) (t1,t2)
+    | _ -> assert false
+  in
+  {env with label_assoc=aux_label [] lassoc;
+	    term_assoc=aux_arg [] (li.l_profile, params);}
+
+and pred env = function
   | Pfalse -> Pfalse
   | Ptrue -> Ptrue
-  | Papp(l,_,_) when List.mem_assoc l.l_var_info vp ->List.assoc l.l_var_info vp
-  | Papp (li,lassoc,params) ->
-    let new_labels, new_args = self#app ll vt vv li lassoc params in
-    begin match li.l_body with
-    | LBnone -> pred (* TODO *)
-    | LBreads _ -> pred (* TODO *)
-    | LBterm _ -> assert false (* unreachable *)
-    | LBpred {content=p} -> self#pred p new_labels new_args vp vv
-    | LBinductive _ -> pred (* TODO *)
+  | Papp(l,_,_) when List.mem_assoc l.l_var_info env.pred_assoc ->
+     List.assoc l.l_var_info env.pred_assoc
+  | Papp (li,lassoc,params) as pred' ->
+    let new_env = app env li lassoc params in
+    begin
+      match li.l_body with
+      | LBnone -> pred' (* TODO *)
+      | LBreads _ -> pred' (* TODO *)
+      | LBterm _ -> assert false (* unreachable *)
+      | LBpred {content=p} -> pred new_env p
+      | LBinductive _ -> pred' (* TODO *)
     end
-  | Pseparated t -> Pseparated (List.map (fun x -> self#term x ll vt vv) t)
-  | Prel(rel,t1,t2) -> Prel(rel, self#term t1 ll vt vv, self#term t2 ll vt vv)
-  | Pand(p1,p2) -> Pand(self#pnamed p1 ll vt vp vv,self#pnamed p2 ll vt vp vv)
-  | Por(p1,p2) -> Por (self#pnamed p1 ll vt vp vv, self#pnamed p2 ll vt vp vv)
-  | Pxor(p1,p2) -> Pxor(self#pnamed p1 ll vt vp vv,self#pnamed p2 ll vt vp vv)
-  | Pimplies(p1,p2) -> Pimplies(self#pnamed p1 ll vt vp vv,
-				self#pnamed p2 ll vt vp vv)
-  | Piff(p1,p2) -> Piff(self#pnamed p1 ll vt vp vv,self#pnamed p2 ll vt vp vv)
-  | Pnot p -> Pnot (self#pnamed p ll vt vp vv)
-  | Pif (t,p1,p2) -> Pif (self#term t ll vt vv, self#pnamed p1 ll vt vp vv,
-			  self#pnamed p2 ll vt vp vv)
-  | Plet (li,{content=p}) ->
+  | Pseparated t -> Pseparated (List.map (fun x -> term env x) t)
+  | Prel(rel,t1,t2) -> Prel(rel, term env t1, term env t2)
+  | Pand(p1,p2) -> Pand(pnamed env p1, pnamed env p2)
+  | Por(p1,p2) -> Por (pnamed env p1, pnamed env p2)
+  | Pxor(p1,p2) -> Pxor(pnamed env p1, pnamed env p2)
+  | Pimplies(p1,p2) -> Pimplies(pnamed env p1, pnamed env p2)
+  | Piff(p1,p2) -> Piff(pnamed env p1, pnamed env p2)
+  | Pnot p -> Pnot (pnamed env p)
+  | Pif (t,p1,p2) -> Pif (term env t, pnamed env p1, pnamed env p2)
+  | Plet (li,{content=p}) as pred' ->
     let lv = li.l_var_info in
-    begin match li.l_body with
-    | LBnone -> pred (* TODO *)
-    | LBreads _ -> pred (* TODO *)
-    | LBterm t' -> self#pred p ll ((lv,(self#term t' ll vt vv))::vt) vp vv
-    | LBpred {content=x} ->
-      self#pred p ll vt ((lv,self#pred x ll vt vp vv)::vp) vv
-    | LBinductive _ -> pred (* TODO *)
+    begin
+      match li.l_body with
+      | LBnone -> pred' (* TODO *)
+      | LBreads _ -> pred' (* TODO *)
+      | LBterm t' ->
+	 pred {env with term_assoc=((lv,(term env t'))::env.term_assoc)} p
+      | LBpred {content=x} ->
+	 pred {env with pred_assoc=((lv,pred env x)::env.pred_assoc)} p
+      | LBinductive _ -> pred' (* TODO *)
     end
-  | Pforall (q,p) | Pexists(q,p) ->
+  | Pforall (q,p)
+  | Pexists (q,p) as pred' ->
     let prefix v = {v with lv_name = "__q_" ^ v.lv_name} in
     let rec aux ret1 ret2 = function
       | [] -> ret1,ret2
       | h::t -> aux ((prefix h)::ret1) ((h, prefix h)::ret2) t
     in
     let new_q, new_quantifs = aux [] [] q in
-    let new_quantifs = List.rev_append new_quantifs vv in
-    let new_p = self#pnamed p ll vt vp new_quantifs in
-    begin match pred with
-    | Pforall _ -> Pforall (new_q, new_p)
-    | _ -> Pexists (new_q, new_p)
+    let new_quantifs = List.rev_append new_quantifs env.var_assoc in
+    let env = {env with var_assoc=new_quantifs} in
+    let new_p = pnamed env p in
+    begin
+      match pred' with
+      | Pforall _ -> Pforall (new_q, new_p)
+      | _ -> Pexists (new_q, new_p)
     end
-  | Pat (p,l) -> Pat (self#pnamed p ll vt vp vv, self#label l ll)
-  | Pvalid_read (l,t) -> Pvalid_read (self#label l ll, self#term t ll vt vv)
-  | Pvalid (l,t) -> Pvalid (self#label l ll, self#term t ll vt vv)
-  | Pinitialized (l,t) -> Pinitialized(self#label l ll, self#term t ll vt vv)
-  | Pdangling (l,t) -> Pdangling(self#label l ll, self#term t ll vt vv)
-  | Pallocable (l,t) -> Pallocable (self#label l ll, self#term t ll vt vv)
-  | Pfreeable (l,t) -> Pfreeable (self#label l ll, self#term t ll vt vv)
-  | Pfresh(l1,l2,t1,t2) -> Pfresh(self#label l1 ll,self#label l2 ll,
-				  self#term t1 ll vt vv,self#term t2 ll vt vv)
-  | Psubtype(t1,t2) -> Psubtype (self#term t1 ll vt vv, self#term t2 ll vt vv)
+  | Pat (p,l) -> Pat (pnamed env p, label env l)
+  | Pvalid_read (l,t) -> Pvalid_read (label env l, term env t)
+  | Pvalid (l,t) -> Pvalid (label env l, term env t)
+  | Pinitialized (l,t) -> Pinitialized(label env l, term env t)
+  | Pdangling (l,t) -> Pdangling(label env l, term env t)
+  | Pallocable (l,t) -> Pallocable (label env l, term env t)
+  | Pfreeable (l,t) -> Pfreeable (label env l, term env t)
+  | Pfresh(l1,l2,t1,t2) -> Pfresh(label env l1, label env l2,
+				  term env t1, term env t2)
+  | Psubtype(t1,t2) -> Psubtype (term env t1, term env t2)
 
-  method tnode term ll vt vv = match term with
+and tnode env = function
   | TConst c -> TConst c
   | TLval (TVar v,y) ->
-    let off = self#toffset y ll vt vv in
-    if List.mem_assoc v vt then
-      let t' = List.assoc v vt in
-      match t'.term_node with
-      | TLval v' -> TLval (Logic_const.addTermOffsetLval off v')
-      | whatever -> assert (off = TNoOffset); whatever
-    else TLval (TVar (if List.mem_assoc v vv then List.assoc v vv else v), off)
-  | TLval(TResult _,y) when subst_result <> None ->
-    (* substitution of \result by a lvalue *)
-    let lval = Extlib.the subst_result in
-    let tlval = Logic_utils.lval_to_term_lval ~cast:false lval in
-    let x = Logic_const.addTermOffsetLval (self#toffset y ll vt vv) tlval in
-    TLval(x)
-  | TLval(TResult t,y) -> TLval(TResult t, self#toffset y ll vt vv)
-  | TLval(TMem t,y) -> TLval(TMem(self#term t ll vt vv),self#toffset y ll vt vv)
+     let off = toffset env y in
+     if List.mem_assoc v env.term_assoc then
+       let t' = List.assoc v env.term_assoc in
+       match t'.term_node with
+       | TLval v' -> TLval (Logic_const.addTermOffsetLval off v')
+       | whatever -> assert (off = TNoOffset); whatever
+     else
+       TLval (TVar (if List.mem_assoc v env.var_assoc then
+		      List.assoc v env.var_assoc else v), off)
+  | TLval(TResult t,y) -> TLval(TResult t, toffset env y)
+  | TLval(TMem t,y) -> TLval(TMem(term env t), toffset env y)
   | TSizeOf t -> TSizeOf t
-  | TSizeOfE t -> TSizeOfE (self#term t ll vt vv)
+  | TSizeOfE t -> TSizeOfE (term env t)
   | TSizeOfStr s -> TSizeOfStr s
   | TAlignOf t -> TAlignOf t
-  | TAlignOfE t -> TAlignOfE (self#term t ll vt vv)
-  | TUnOp (u,t) -> TUnOp (u, self#term t ll vt vv)
-  | TBinOp(b,t1,t2) -> TBinOp(b, self#term t1 ll vt vv, self#term t2 ll vt vv)
-  | TCastE (ty,t) -> TCastE (ty, self#term t ll vt vv)
-  | TAddrOf _ -> term (* TODO *)
-  | TStartOf _ -> term (* TODO *)
+  | TAlignOfE t -> TAlignOfE (term env t)
+  | TUnOp (u,t) -> TUnOp (u, term env t)
+  | TBinOp(b,t1,t2) -> TBinOp(b, term env t1, term env t2)
+  | TCastE (ty,t) -> TCastE (ty, term env t)
+  | TAddrOf _ as term' -> term' (* TODO *)
+  | TStartOf _ as term' -> term' (* TODO *)
   | Tapp (li,[],[lower;upper;({term_node=Tlambda([_],_)} as lambda)]) ->
-    Tapp (li,[],List.map (fun x -> self#term x ll vt vv) [lower; upper; lambda])
-  | Tapp (li,lassoc,params) ->
-    let new_labels, new_args = self#app ll vt vv li lassoc params in
-    let new_terms = List.map (fun x -> self#term x ll vt vv) params in
-    begin match li.l_body with
-    | LBnone ->
-      let s = li.l_var_info.lv_name in
-      if s = "\\cos" || s = "\\abs" || s = "\\sqrt" || s = "\\pow" then
-	Tapp(li,new_labels,new_terms)
-      else term (* TODO *)
-    | LBreads _ -> term (* TODO *)
-    | LBterm {term_node = t} -> self#tnode t new_labels new_args vv
-    | LBpred _ -> assert false (* unreachable *)
-    | LBinductive _ -> term (* TODO *)
+    Tapp (li,[],List.map (fun x -> term env x) [lower; upper; lambda])
+  | Tapp (li,lassoc,params) as term' ->
+    let env = app env li lassoc params in
+    let new_terms = List.map (fun t -> term env t) params in
+    begin
+      match li.l_body with
+      | LBnone ->
+	 let s = li.l_var_info.lv_name in
+	 if s = "\\cos" || s = "\\abs" || s = "\\sqrt" || s = "\\pow" then
+	   Tapp(li,env.label_assoc,new_terms)
+	 else term' (* TODO *)
+      | LBreads _ -> term' (* TODO *)
+      | LBterm {term_node = t} -> tnode env t
+      | LBpred _ -> assert false (* unreachable *)
+      | LBinductive _ -> term' (* TODO *)
     end
-  | Tlambda (q,t) -> Tlambda (q, self#term t ll vt vv)
-  | TDataCons _ -> term (* TODO *)
-  | Tif(t1,t2,t3) -> Tif(self#term t1 ll vt vv, self#term t2 ll vt vv,
-			 self#term t3 ll vt vv)
-  | Tat (t,l) -> Tat (self#term t ll vt vv, self#label l ll)
-  | Tbase_addr (l,t) -> Tbase_addr (self#label l ll, self#term t ll vt vv)
-  | Toffset (l,t) -> Toffset (self#label l ll, self#term t ll vt vv)
-  | Tblock_length(l,t)-> Tblock_length(self#label l ll, self#term t ll vt vv)
+  | Tlambda (q,t) -> Tlambda (q, term env t)
+  | TDataCons _ as term' -> term' (* TODO *)
+  | Tif(t1,t2,t3) -> Tif(term env t1, term env t2, term env t3)
+  | Tat (t,l) -> Tat (term env t, label env l)
+  | Tbase_addr (l,t) -> Tbase_addr (label env l, term env t)
+  | Toffset (l,t) -> Toffset (label env l, term env t)
+  | Tblock_length(l,t)-> Tblock_length(label env l, term env t)
   | Tnull -> Tnull
-  | TLogic_coerce(y,t)-> TLogic_coerce(y, self#term t ll vt vv)
-  | TCoerce (t, ty) -> TCoerce (self#term t ll vt vv, ty)
-  | TCoerceE(t1, t2) -> TCoerceE(self#term t1 ll vt vv, self#term t2 ll vt vv)
-  | TUpdate(t1,o,t2) -> TUpdate(self#term t1 ll vt vv,self#toffset o ll vt vv,
-				self#term t2 ll vt vv)
-  | Ttypeof t -> Ttypeof (self#term t ll vt vv)
+  | TLogic_coerce(y,t)-> TLogic_coerce(y, term env t)
+  | TCoerce (t, ty) -> TCoerce (term env t, ty)
+  | TCoerceE(t1, t2) -> TCoerceE(term env t1, term env t2)
+  | TUpdate(t1,o,t2) -> TUpdate(term env t1, toffset env o, term env t2)
+  | Ttypeof t -> Ttypeof (term env t)
   | Ttype t -> Ttype t
   | Tempty_set -> Tempty_set
-  | Tunion l -> Tunion (List.map (fun x -> self#term x ll vt vv) l)
-  | Tinter l -> Tinter (List.map (fun x -> self#term x ll vt vv) l)
-  | Tcomprehension (t,q,None) -> Tcomprehension(self#term t ll vt vv, q, None)
-  | Tcomprehension _ -> term
+  | Tunion l -> Tunion (List.map (fun x -> term env x) l)
+  | Tinter l -> Tinter (List.map (fun x -> term env x) l)
+  | Tcomprehension (t,q,None) -> Tcomprehension(term env t, q, None)
+  | Tcomprehension _ as term' -> term'
   | Trange(None, None) -> Trange(None, None)
-  | Trange(None,Some t)-> Trange(None, Some(self#term t ll vt vv))
-  | Trange(Some t,None)-> Trange(Some(self#term t ll vt vv), None)
-  | Trange(Some t1, Some t2) ->
-    Trange(Some(self#term t1 ll vt vv), Some(self#term t2 ll vt vv))
-  | Tlet (li,{term_node=t}) ->
+  | Trange(None,Some t)-> Trange(None, Some(term env t))
+  | Trange(Some t,None)-> Trange(Some(term env t), None)
+  | Trange(Some t1, Some t2) -> Trange(Some(term env t1), Some(term env t2))
+  | Tlet (li,{term_node=t}) as term' ->
     let lv = li.l_var_info in
     match li.l_body with
-    | LBnone -> term (* TODO *)
-    | LBreads _ -> term (* TODO *)
-    | LBterm t' -> self#tnode t ll ((lv, (self#term t' ll vt vv))::vt) vv
+    | LBnone -> term' (* TODO *)
+    | LBreads _ -> term' (* TODO *)
+    | LBterm t' ->
+       tnode {env with term_assoc=((lv, (term env t'))::env.term_assoc)} t
     | LBpred _ -> assert false (* unreachable *)
-    | LBinductive _ -> term (* TODO *)
+    | LBinductive _ -> term' (* TODO *)
 
-  method toffset offset ll vt vv = match offset with
+and toffset env = function
   | TNoOffset -> TNoOffset
-  | TField (f,o) -> TField (f, self#toffset o ll vt vv)
-  | TModel (m,o) -> TModel (m, self#toffset o ll vt vv)
-  | TIndex (t,o) -> TIndex (self#term t ll vt vv, self#toffset o ll vt vv)
+  | TField (f,o) -> TField (f, toffset env o)
+  | TModel (m,o) -> TModel (m, toffset env o)
+  | TIndex (t,o) -> TIndex (term env t, toffset env o)
 
-  method term t ll vt vv = {t with term_node=self#tnode t.term_node ll vt vv}
-  method pnamed p ll vt vp vv = {p with content=self#pred p.content ll vt vp vv}
-  method label l ll = if List.mem_assoc l ll then List.assoc l ll else l
-end
+and term env t = {t with term_node=tnode env t.term_node}
+
+and pnamed env p = {p with content=pred env p.content}
+
+and id_pred env p = {p with ip_content=pred env p.ip_content}
+
 
 
 
 class inline_inplace = object
   inherit Visitor.frama_c_inplace
 
-  method! private vpredicate_named p =
-    Cil.ChangeTo ((new subst ())#pnamed p [] [] [] [])
+  method! private vpredicate_named p = Cil.ChangeTo (pnamed empty_env p)
 
-  method! private vterm t =
-    Cil.ChangeTo ((new subst ())#term t [] [] [])
+  method! private vterm t = Cil.ChangeTo (term empty_env t)
 
   method! private vglob_aux = function
     | GAnnot _ -> Cil.ChangeTo []
